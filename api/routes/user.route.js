6f3b10a4f79f8d2790d22a2f8af44a90bd7d0804
shreyasri258 <kertishreya2508@gmail.com>
2024-05-31 22:53:43 +0530

    Added signout functionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { test } from '../controllers/user.controller.js';
-import { signup } from '../controllers/auth.controllers.js';
 import { updateUser } from '../controllers/user.controller.js';
 import { deleteUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
-router.post('/signup',signup);
-router.post('/update/:id',verifyToken,updateUser);
-router.delete('/delete/:id',verifyToken,deleteUser);
+
+router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -9,7 +9,7 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
-import { updateUserStart,updateUserFailure,updateUserSuccess,deleteUserFailure,deleteUserSuccess, deleteUserStart } from "../redux/user/userSlice";
+import { updateUserStart,updateUserFailure,updateUserSuccess,deleteUserFailure,deleteUserSuccess,signOutUserFailure,signOutUserStart,signOutUserSuccess, deleteUserStart } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
 
 export default function Profile() {
@@ -95,6 +95,20 @@ export default function Profile() {
       dispatch(deleteUserFailure(error.message));
     }
   };
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7"> Profile</h1>
@@ -154,7 +168,7 @@ export default function Profile() {
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
-        <span className="text-red-700 cursor-pointer">Sign Out</span>
+        <span onClick={handleSignOut} className="text-red-700 cursor-pointer">Sign Out</span>
       </div>
       <p className="text-red-700 mt-5">{error?error:''}</p>
       <p className="text-green-700 mt-5">{updateSuccess?'User is updated successfully':''}</p>
