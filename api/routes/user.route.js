624fc47a94234234ca4366391b23ccebf6d390bb
shreyasri258 <kertishreya2508@gmail.com>
2024-06-03 22:12:17 +0530

    Added Listing and Contact Landloard pages

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,7 +3,8 @@ import { test } from '../controllers/user.controller.js';
 import { updateUser } from '../controllers/user.controller.js';
 import { deleteUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
-import { getUserListings } from '../controllers/user.controller.js';
+import { getUserListings ,getUser} from '../controllers/user.controller.js';
+
 const router = express.Router();
 
 
@@ -11,4 +12,5 @@ router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
 router.get('/listings/:id',verifyToken,getUserListings);
+router.get('/:id',verifyToken,getUser);
 export default router;
