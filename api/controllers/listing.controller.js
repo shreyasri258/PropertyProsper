f350ad36b0e94de2897fa7ab4ce51703c9b28a4d
shreyasri258 <kertishreya2508@gmail.com>
2024-06-03 20:48:26 +0530

    Created and updated Listing page

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req,res,next)=>{
     try {
@@ -47,4 +48,14 @@ export const deleteListing = async (req, res, next) => {
       next(error);
     }
   };
-  
+  export const getListing = async (req, res, next) => {
+    try {
+      const listing = await Listing.findById(req.params.id);
+      if (!listing) {
+        return next(errorHandler(404, 'Listing not found!'));
+      }
+      res.status(200).json(listing);
+    } catch (error) {
+      next(error);
+    }
+  };
