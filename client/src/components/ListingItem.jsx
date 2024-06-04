c4c9f97471e7416f146f2bceda2205d7c97a88e5
shreyasri258 <kertishreya2508@gmail.com>
2024-06-04 09:52:29 +0530

    Added About page and fixed errors

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -29,8 +29,8 @@ export default function ListingItem({ listing }) {
           <p className='text-slate-500 mt-2 font-semibold '>
             $
             {listing.offer
-              ? listing.discountPrice.toLocaleString('en-US')
-              : listing.regularPrice.toLocaleString('en-US')}
+              ? listing.discountPrice
+              : listing.regularPrice}
             {listing.type === 'rent' && ' / month'}
           </p>
           <div className='text-slate-700 flex gap-4'>
